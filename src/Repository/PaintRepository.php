3c7671f79c26356279c297bff5886c795bed2483
Pegasus03 <estimejesus@gmail.com>
2022-07-10 08:50:51 +0200

    add 3 articles and 3 paints for homepage

--- src/Repository/PaintRepository.php
+++ src/Repository/PaintRepository.php
@@ -39,20 +39,18 @@ class PaintRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Paint[] Returns an array of Paint objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+   /**
+    * @return Paint[] Returns an array of Paint objects
+    */
+   public function lastTree()
+   {
+       return $this->createQueryBuilder('p')
+           ->orderBy('p.id', 'DESC')
+           ->setMaxResults(3)
+           ->getQuery()
+           ->getResult()
+       ;
+   }
 
 //    public function findOneBySomeField($value): ?Paint
 //    {
